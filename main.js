cc068067b4c3ef329f61821cf7786738a4f025f0
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-10-27 23:12:43 +0530

    basic spehere

---
+++ main.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three';
+import "./style.css"
+import gsap from "gsap"
+import {OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: '#00ff83',
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const light = new THREE.PointLight(0xffffff, 70, 100, 1.7)
+light.position.set(0, 10, 10)
+scene.add(light)
+
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
+renderer.render(scene, camera)
+
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+})
+
+const loop = () =>{
+  // mesh.position.x += 0.1
+  controls.update()
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+
+}
+loop()
+
+//timline magik
+const tl = gsap.timeline({defaults: { duration : 1}})
+tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1})
+tl.fromTo("nav",{ y:"-100%"},{y:"0%"})
+tl.fromTo(".title",{opacity: 0},{opacity: 1})
+
